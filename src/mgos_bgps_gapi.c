e1d60ef1968dd32a4be535a361f659c6f282f811
fbalestrini <gaojing@hotmail.it>
2022-07-19 16:22:35 +0200

    Updated

--- src/mgos_bgps_gapi.c
+++ src/mgos_bgps_gapi.c
@@ -10,10 +10,10 @@ static char *s_api_url = NULL;
 
 static bool s_requesting = false;
 
+static int s_update_timer_id = MGOS_INVALID_TIMER_ID;
+
 static bool s_position_ok = false;
-static float s_latitude = 0.0;
-static float s_longitude = 0.0;
-static float s_accuracy = 0;
+static struct mgos_bgps_position s_position;
 
 int mg_wifi_scan_result_to_json(struct json_out *out, va_list *ap) {
   struct mgos_wifi_scan_result *aps = va_arg(*ap, struct mgos_wifi_scan_result *);
@@ -70,7 +70,6 @@ static void mg_bgps_gapi_http_cb(struct mg_connection *c, int ev, void *ev_data,
         LOG(LL_ERROR,("HTTPS error %d (response: '%s')", hm->resp_code, hm->body.p));
         s_position_ok = false;
       }
-      s_requesting = false;
       break;
     case MG_EV_CLOSE:
       s_requesting = false;
@@ -78,18 +77,16 @@ static void mg_bgps_gapi_http_cb(struct mg_connection *c, int ev, void *ev_data,
   }
 }
 
-static bool mg_bgps_gapi_start_get_position(int aps_len, struct mgos_wifi_scan_result *aps) {
+static bool mg_bgps_gapi_start_invoke_api(int aps_len, struct mgos_wifi_scan_result *aps) {
   bool success = false;
 
   char *request_body = json_asprintf("{considerIp: false, wifiAccessPoints: %M}",
     mg_wifi_scan_result_to_json, aps, aps_len);
 
-  s_requesting = true;
-  success = mg_connect_http(mgos_get_mgr(), mg_bgps_gapi_http_cb, NULL, s_api_url,
-    NULL, request_body);
+  success = mg_connect_http(mgos_get_mgr(), mg_bgps_gapi_http_cb,
+    NULL, s_api_url, NULL, request_body);
 
   if (!success) {
-    s_requesting = false;
     LOG(LL_ERROR,("POST request to %s failed (body: '%s')", s_api_url, request_body));
   }
 
@@ -99,28 +96,65 @@ static bool mg_bgps_gapi_start_get_position(int aps_len, struct mgos_wifi_scan_r
 
 static void mg_bgps_gapi_wifi_scan_cb(int n, struct mgos_wifi_scan_result *res, void *arg) {
   if (mgos_wifi_get_status() == MGOS_WIFI_IP_ACQUIRED) {
-    if (!mg_bgps_gapi_start_get_position(n, res)) {
-      s_position_ok = false;
+    if (mg_bgps_gapi_start_invoke_api(n, res)) {
+      return;
     }
-  } else {
-    s_position_ok = false;
   }
+
+  s_requesting = false;
+  s_position_ok = false;
+
   (void) arg;
 }
 
-static void mg_bgps_gapi_timer_cb(void *arg) {
+static bool mg_bgps_gapi_start_get_position() {
   if (!s_requesting) {
+    s_requesting = true;
     mgos_wifi_scan(mg_bgps_gapi_wifi_scan_cb, NULL);
+    return true;
   }
+  return false;
+}
+
+static void mg_bgps_gapi_update_timer_cb(void *arg) {
+  mg_bgps_gapi_start_get_position();
   (void) arg;
 }
 
-bool mgos_bgps_get_position(float *latitude, float *longitude, float *accuracy) {
+bool mgos_bgps_get_position(struct mgos_bgps_position *position) {
+  if (!position) return false;
   // initialize output
-  if (latitude) *latitude = (s_position_ok ? s_latitude : 0.0);
-  if (longitude) *longitude = (s_position_ok ? s_longitude : 0.0);
-  if (accuracy) *accuracy = (s_position_ok ? s_accuracy : 0);
+  position->location.latitude = (s_position_ok ? s_position.location.latitude : 0.0);
+  position->location.longitude = (s_position_ok ? s_position.location.longitude : 0.0);
+  position->accuracy = (s_position_ok ? s_position.accuracy : 0.0);
   return s_position_ok;
+}
+
+static void mg_bgps_gapi_net_ev_handler(int ev, void *evd, void *arg) {
+  switch(ev) {
+    case MGOS_NET_EV_IP_ACQUIRED:
+      // Tyr to update immediately the position
+      if (s_update_timer_id == MGOS_INVALID_TIMER_ID) {
+        mg_bgps_gapi_start_get_position();
+      }
+      // Start the update timer
+      if ((mgos_sys_config_get_gps_gapi_update_interval() > 0) &&
+          (s_update_timer_id == MGOS_INVALID_TIMER_ID)) {
+        s_update_timer_id = mgos_set_timer(mgos_sys_config_get_gps_gapi_update_interval(),
+          MGOS_TIMER_REPEAT | MGOS_TIMER_RUN_NOW, mg_bgps_gapi_update_timer_cb, NULL);
+      }
+      break;
+    case MGOS_NET_EV_DISCONNECTED:
+      // Stop the update timer
+      if (s_update_timer_id != MGOS_INVALID_TIMER_ID) {
+        mgos_clear_timer(s_update_timer_id);
+        s_update_timer_id = MGOS_INVALID_TIMER_ID;
+      }
+      break;
+  }
+
+  (void) evd;
+  (void) arg;
 }
 
 #ifdef MGOS_HAVE_MJS
@@ -128,9 +162,13 @@ bool mgos_bgps_get_position(float *latitude, float *longitude, float *accuracy) 
 #endif /* MGOS_HAVE_MJS */
 
 bool mgos_bgps_gapi_init() {
+  // Initializa position
+  s_position.location.latitude = 0.0;
+  s_position.location.longitude = 0.0;
+  s_position.accuracy = 0.0;
 
-  // Initialize the Google API URL
-  // like https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
+  // Initialize the Google Geolocate API URL
+  // (e.g.: https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY)
   if (mgos_sys_config_get_gps_gapi_api_key() != NULL &&
       mgos_sys_config_get_gps_gapi_url() != NULL) {
     size_t len = strlen(mgos_sys_config_get_gps_gapi_api_key()) +
@@ -141,18 +179,15 @@ bool mgos_bgps_gapi_init() {
       mgos_sys_config_get_gps_gapi_url(),
       mgos_sys_config_get_gps_gapi_api_key());
 
-    LOG(LL_INFO,("Google Geolocate API URL: %s", s_api_url));
+    LOG(LL_DEBUG,("Google Geolocate API URL: %s", s_api_url));
   } else {
     LOG(LL_ERROR,("Invalid empty API-KEY and/or API-URL"));
   }
 
-  // Start the polling interval
-  if (s_api_url && (mgos_sys_config_get_gps_gapi_update_interval() > 0)) {
-    mgos_set_timer(mgos_sys_config_get_gps_gapi_update_interval(),
-      MGOS_TIMER_REPEAT | MGOS_TIMER_RUN_NOW,
-      mg_bgps_gapi_timer_cb, NULL);
+  if (s_api_url) {
+    mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, mg_bgps_gapi_net_ev_handler, NULL);
   } else {
-    LOG(LL_ERROR,("Update interval not set"));
+    LOG(LL_ERROR,("Invalid empty Google Geolocate API URL"));
   }
 
   return true;
