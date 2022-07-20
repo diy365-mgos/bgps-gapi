ba490e74b03f1a9eeb555da036d6709cc36a8bad
fbalestrini <gaojing@hotmail.it>
2022-07-20 14:31:37 +0200

    Updated

--- src/mgos_bgps_gapi.c
+++ src/mgos_bgps_gapi.c
@@ -75,6 +75,9 @@ static void mg_bgps_gapi_http_cb(struct mg_connection *c, int ev, void *ev_data,
         s_position_ok = false;
       }
       s_requesting = false;
+      // force the connection to be closed to avoid
+      // memory saturation in case of multiple requests
+      nc->flags |= MG_F_SEND_AND_CLOSE;
       break;
   }
 }
@@ -86,7 +89,7 @@ static bool mg_bgps_gapi_start_invoke_api(int aps_len, struct mgos_wifi_scan_res
     mg_wifi_scan_result_to_json, aps, aps_len);
 
   success = mg_connect_http(mgos_get_mgr(), mg_bgps_gapi_http_cb,
-    NULL, s_api_url, NULL, request_body);
+    NULL, s_api_url, "Content-Type: application/json\r\n", request_body);
 
   if (!success) {
     LOG(LL_ERROR,("POST request to %s failed (body: '%s')", s_api_url, request_body));
